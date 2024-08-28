5bbcd2faca310918801eb2b2a980fd1f76c786dd
T S <cactoid@gmail.com>
2024-08-28 23:56:41 +0900

    up

--- argobots/src/include/abtd_stream.h
+++ argobots/src/include/abtd_stream.h
@@ -10,19 +10,19 @@
 ABTU_ret_err static inline int
 ABTD_xstream_barrier_init(uint32_t num_waiters, ABTD_xstream_barrier *p_barrier)
 {
-    int ret = pthread_barrier_init(p_barrier, NULL, num_waiters);
+    int ret = real_pthread_barrier_init(p_barrier, NULL, num_waiters);
     return (ret == 0) ? ABT_SUCCESS : ABT_ERR_XSTREAM_BARRIER;
 }
 
 static inline void ABTD_xstream_barrier_destroy(ABTD_xstream_barrier *p_barrier)
 {
-    int ret = pthread_barrier_destroy(p_barrier);
+    int ret = real_pthread_barrier_destroy(p_barrier);
     ABTI_ASSERT(ret == 0);
 }
 
 static inline void ABTD_xstream_barrier_wait(ABTD_xstream_barrier *p_barrier)
 {
-    int ret = pthread_barrier_wait(p_barrier);
+    int ret = real_pthread_barrier_wait(p_barrier);
     ABTI_ASSERT(ret == PTHREAD_BARRIER_SERIAL_THREAD || ret == 0);
 }
 #endif
